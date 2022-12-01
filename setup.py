e0ac511a8194db248a423299e9e906f09294a21b
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-01 09:05:37 -0500

    chore: updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="stew-hash",
-    version="0.1.0",
+    version="0.2.0",
     author="apinanyogaratnam",
     author_email="apinanapinan@icloud.com",
     description="A simple hashing library",
